fdd33e84afe7fd9cb6de5449921d54768e436206
Matias Martinez <mat.360z@gmail.com>
2017-11-06 20:16:01 -0300

    Soporte para la resta

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -19,8 +19,13 @@ public class Calculadora {
             try {
                 int op1 = Integer.parseInt(args[1]);
                 int op2 = Integer.parseInt(args[2]);
-
-                System.out.println(op1 + op2);
+                if("+".equals(args[0])){
+                   System.out.println(op1 + op2);
+                }else if("-".equals(args[0])){
+                    System.out.println(op1 - op2);
+                }else{
+                    System.out.println(args[0] + ": operación desconocida.");
+                }                
             }catch(Exception ex){
                 System.out.println("Parametros incorrectos");
             }
